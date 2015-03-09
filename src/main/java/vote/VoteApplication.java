0af71fd6ca0c73245a6922c4e50167bd41a16e12
anupkher4 <anup.kher.1990@gmail.com>
2015-03-08 22:30:08 -0700

    initial commit

---
+++ src/main/java/vote/VoteApplication.java
@@ -0,0 +1,15 @@
+package vote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by anupkher on 3/4/15.
+ */
+
+@SpringBootApplication
+public class VoteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VoteApplication.class, args);
+    }
+}
